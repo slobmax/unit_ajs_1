51aa6cf643a813fb4ff4935a428e84b97cd53c7a
Maxim Slobozhanin <slob.max@gmail.com>
2022-11-22 02:15:58 +0400

    task completed

---
+++ src/index.js
@@ -0,0 +1,16 @@
+export default function healthStatus(options) {
+  let result;
+  if (options.health <= 30) {
+    result = 'critical';
+  };
+  
+  if (options.health <= 70 && options.health > 30) {
+    result = 'wounded';
+  };
+  
+  if (options.health <= 100 && options.health > 70) {
+    result = 'healthy';
+  };
+  
+  return result;
+};
---
+++ test/test.js
@@ -0,0 +1,13 @@
+import healthStatus from '../src/index.js';
+
+test('healthStatus', () => {    
+  expect(healthStatus({ name: 'мечник', health: 10 })).toBe('critical');
+});
+
+test('healthStatus', () => {  
+  expect(healthStatus({ name: 'маг', health: 50 })).toBe('wounded');
+});
+
+test('healthStatus', () => {   
+  expect(healthStatus({ name: 'лучник', health: 80 })).toBe('healthy');
+});
