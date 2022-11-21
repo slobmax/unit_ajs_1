3bea7b9c6d9d7004f2681285523d94ed3713720a
Maxim Slobozhanin <slob.max@gmail.com>
2022-11-22 02:39:48 +0400

    edited conditions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 export default function healthStatus(options) {
   let result;
-  if (options.health <= 30) {
+  if (options.health <= 15) {
     result = 'critical';
   }
-  if (options.health <= 70 && options.health > 30) {
+  if (options.health <= 50 && options.health > 15) {
     result = 'wounded';
   }
-  if (options.health <= 100 && options.health > 70) {
+  if (options.health <= 100 && options.health > 50) {
     result = 'healthy';
   }
   return result;
