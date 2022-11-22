63a9acba2631ab2f276ffd4d91561ee7e1069a51
Maxim Slobozhanin <slob.max@gmail.com>
2022-11-22 15:09:26 +0400

    modified test.js

--- test/test.js
+++ test/test.js
@@ -2,12 +2,6 @@ import healthStatus from '../src/index';
 
 test('healthStatus', () => {
   expect(healthStatus({ name: 'мечник', health: 10 })).toBe('critical');
-});
-
-test('healthStatus', () => {
   expect(healthStatus({ name: 'маг', health: 50 })).toBe('wounded');
-});
-
-test('healthStatus', () => {
   expect(healthStatus({ name: 'лучник', health: 80 })).toBe('healthy');
 });
